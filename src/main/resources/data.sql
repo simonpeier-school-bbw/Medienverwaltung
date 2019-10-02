ff365bd1f957efd95e84f8262e26283785046b59
Simon Peier <simon.w.peier@gmail.com>
2019-10-02 16:25:04 +0200

    Add example data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,12 @@
-insert into entry (check_in , check_out)
-values (parsedatetime('12-02-2009 23:33:22.00', 'dd-MM-yyyy HH:mm:ss.SS'), parsedatetime('12-02-2019 23:33:22.00','dd-MM-yyyy HH:mm:ss.SS'));
-
 INSERT INTO user (id, name , surname, username, password)
 VALUES (1, 'hans', 'meier', 'user', '$2a$10$jgK/iszM3xKjkzcu0qH4wO.miIZ/drwybwVyp.3xSjyO5BdtKepLy');
 -- password: password
 
 INSERT INTO user_roles (user_id, roles)
 VALUES (1, 'ROLE_USER');
+
+INSERT INTO medium (id, title ,publisher ,rating ,description)
+VALUES (1, 'Terry Botter', 'halo', 'good', 'cool game!');
+
+INSERT INTO book (id,author,pages, medium_id, user_id)
+VALUES (1, 'D.J. Bowling', '300',1,1)
