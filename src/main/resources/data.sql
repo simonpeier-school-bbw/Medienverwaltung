811d00acaebced77fa81b83ababe895c712b5a99
Simon Peier <simon.w.peier@gmail.com>
2019-10-02 09:44:46 +0200

    Update data.sql according to data class User

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into entry (check_in , check_out)
 values (parsedatetime('12-02-2009 23:33:22.00', 'dd-MM-yyyy HH:mm:ss.SS'), parsedatetime('12-02-2019 23:33:22.00','dd-MM-yyyy HH:mm:ss.SS'));
 
-INSERT INTO user (id, username, password)
-VALUES (1, 'user', '$2a$10$jgK/iszM3xKjkzcu0qH4wO.miIZ/drwybwVyp.3xSjyO5BdtKepLy');
+INSERT INTO user (id, name , surname, username, password)
+VALUES (1, 'hans', 'meier', 'user', '$2a$10$jgK/iszM3xKjkzcu0qH4wO.miIZ/drwybwVyp.3xSjyO5BdtKepLy');
 -- password: password
 
 INSERT INTO user_roles (user_id, roles)
