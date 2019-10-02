5671b15b91b80134561d7931f20dc835a7c43236
Simon Peier <simon.w.peier@gmail.com>
2019-10-02 16:21:53 +0200

    Add special query

--- src/main/java/ch/zli/medienverwaltung/service/BoardGameService.java
+++ src/main/java/ch/zli/medienverwaltung/service/BoardGameService.java
@@ -4,12 +4,18 @@ import ch.zli.medienverwaltung.domain.BoardGame;
 import ch.zli.medienverwaltung.repository.BoardGameRepository;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class BoardGameService {
     private BoardGameRepository boardGameRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public BoardGameService(BoardGameRepository boardGameRepository) {
         this.boardGameRepository = boardGameRepository;
@@ -31,5 +37,13 @@ public class BoardGameService {
         if (boardGameRepository.existsById(id)) {
             boardGameRepository.deleteById(id);
         }
+    }
+
+    public BoardGame findBoardGameByName(String name) {
+        String queryString = "SELECT bg FROM BoardGame as bg WHERE bg.name = :name";
+
+        TypedQuery<BoardGame> query = entityManager.createQuery(queryString, BoardGame.class);
+        query.setParameter("name", name);
+        return (BoardGame) query.getSingleResult();
     }
 }
